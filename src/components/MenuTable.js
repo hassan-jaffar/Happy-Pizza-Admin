ba376aee4d98b1513ab986c143645855857dbcb4
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-09-06 16:21:00 +0500

    INtegrating the getallitems API and updating the ui of accordion

--- src/components/MenuTable.js
+++ src/components/MenuTable.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
+import '../components/Menu.css'
 
 function MenuTable() {
+  const [category, setcategory] = useState(
+    []
+  )
+  const [item, setItem] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+
+        const data = await (await axios.get('http://localhost:5000/api/admin/getallmenu')).data
+        setcategory(data.data);
+        console.log(category)
+
+
+      } catch (error) {
+        console.log(error);
+
+      }
+    }
+    fetchData();
+  }, [])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await (await axios.get('http://localhost:5000/api/admin/getallitems')).data
+        setItem(data.data);
+        console.log(item)
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [])
+
   return (
     <>
       <div className="row sharebox">
@@ -79,78 +117,93 @@ function MenuTable() {
           <hr />
           <br />
 
-          <div className="accordion" id="accordionExample">
+          {category && category.map((categorys) => {
+            return <>
+              <div className="accordion" id={`accordion${categorys.ID}`}>
 
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="headingTwo">
-                <button
-                  className="accordion-button collapsed boldtext"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseTwo"
-                  aria-expanded="false"
-                  aria-controls="collapseTwo"
-                >
-                  Drinks
-                  <div className="mlauto">
-                    <button className="btn btn-info menu-buttons"><i class="fa-solid fa-plus"></i></button>
-                    <button className="btn btn-warning menu-buttons"><i class="fa-solid fa-pencil"></i></button>
-                    <button className="btn btn-danger  menu-buttons"><i class="fa-solid fa-trash"></i></button>
-                    <button className="btn btn-info  menu-buttons"><i class="fa-solid fa-arrow-down"></i></button>
-                    <button className="btn btn-info  menu-buttons"><i class="fa-solid fa-arrow-up"></i></button>
-                  </div>
-                </button>
-              </h2>
-              <div
-                id="collapseTwo"
-                className="accordion-collapse collapse"
-                aria-labelledby="headingTwo"
-                data-bs-parent="#accordionExample"
-              >
-                <div className="accordion-body menudetails">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="menucards">
-                            <div className="row">
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id={`heading${categorys.ID}`}>
+                    <button
+                      className="accordion-button collapsed boldtext"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target={`#collapse${categorys.ID}`}
+                      aria-expanded="false"
+                      aria-controls={`collapse${categorys.ID}`}
+                    >
+                      {categorys.Name}
+                      <div className="mlauto">
+                        <button className="btn btn-info menu-buttons"><i class="fa-solid fa-plus"></i></button>
+                        <button className="btn btn-warning menu-buttons"><i class="fa-solid fa-pencil"></i></button>
+                        <button className="btn btn-danger  menu-buttons"><i class="fa-solid fa-trash"></i></button>
+                        <button className="btn btn-info  menu-buttons"><i class="fa-solid fa-arrow-down"></i></button>
+                        <button className="btn btn-info  menu-buttons"><i class="fa-solid fa-arrow-up"></i></button>
+                      </div>
+                    </button>
+                  </h2>
+                  <div
+                    id={`collapse${categorys.ID}`}
+                    className="accordion-collapse collapse"
+                    aria-labelledby={`heading${categorys.ID}`}
+                    data-bs-parent={`#accordion${categorys.ID}`}
+                  >
+                    <div className="accordion-body menudetails">
+                      <div className="row">
+                        {item && item.map((items) => {
+                          return <>
+                            {items.category_id === categorys.ID ? (<>
+                              <div className="col-md-4">
+                                <div className="menucards">
+                                  <div className="row">
                                     <div className="col-md-12">
-                                    <h5 className="pb-3 d-block mb-5">RICE AND SAUCE</h5>
+                                      <h5 className="pb-3 d-block mb-5">{items.Title}</h5>
                                     </div>
-                                </div>
-                                <div className="row">
+                                  </div>
+                                  <div className="row">
                                     <div className="col-md-6">
-                                        <p>$0.00<br/>AVAILABLE</p>
+                                      <p>{items.Price !== "undefined" ? (<p>$ {items.Price}<br />AVAILABLE</p>) : (<>AVAILABLE</>)}</p>
                                     </div>
                                     <div className="col-md-6 text-end">
-                                        <button className="btn btn-light"><i class="fa-solid fa-camera"></i></button>
+                                      <button className="btn btn-light"><i class="fa-solid fa-camera"></i></button>
                                     </div>
+                                  </div>
                                 </div>
-                            </div>
+                              </div>
+                            </>) : (<>
+
+                            </>)}
+
+                          </>
+                        })}
+
+                      </div>
+                      {/* <div className="row">
+            <div className="col-md-12">
+                <div className="menucards">
+                <div className="row">
+                        <div className="col-md-12">
+                        <h5 className="pb-3 d-block mb-5">RICE AND SAUCE</h5>
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-md-12">
-                            <div className="menucards">
-                            <div className="row">
-                                    <div className="col-md-12">
-                                    <h5 className="pb-3 d-block mb-5">RICE AND SAUCE</h5>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-md-6">
-                                        <p>$0.00<br/>AVAILABLE</p>
-                                    </div>
-                                    <div className="col-md-6 text-end">
-                                        <button className="btn btn-light"><i class="fa-solid fa-camera"></i></button>
-                                    </div>
-                                </div>
-                            </div>
+                        <div className="col-md-6">
+                            <p>$0.00<br/>AVAILABLE</p>
+                        </div>
+                        <div className="col-md-6 text-end">
+                            <button className="btn btn-light"><i class="fa-solid fa-camera"></i></button>
                         </div>
                     </div>
                 </div>
-              </div>
             </div>
+        </div> */}
+                    </div>
+                  </div>
+                </div>
 
-          </div>
+              </div>
+            </>
+          })}
+
         </div>
       </div>
     </>
