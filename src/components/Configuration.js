709827abb7a578fb547f5512861b7cecacd878ea
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-09-21 17:26:33 +0500

    Integrating config API

--- src/components/Configuration.js
+++ src/components/Configuration.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Configuration() {
+  const [order_time, setorder_time] = useState("");
+  const [otp, setotp] = useState("")
+
+  async function register(){
+    const details = {
+      order_time,
+      otp
+    }
+    try {
+
+      const result = await axios.post("http://localhost:5000/api/setting/config",details).data;
+      console.log(result)
+      toast.success("Data has been saved")
+
+      setorder_time("")
+      setotp("")
+
+
+
+  } catch (error) {
+      console.log(error);
+      toast.warn("Something went wrong!")
+
+  }
+  }
   return (
     <>
+           <ToastContainer />
       <h6 className="px-1">CONFIGURATION</h6>
       <hr />
       <br />
@@ -12,8 +41,11 @@ function Configuration() {
           <input
             id="pre-order"
             className="form-control my-2 py-2 w-50"
-            type="text"
+            type="number"
             placeholder="1000"
+            value={order_time}
+            onChange={(e)=>{setorder_time(e.target.value)}}
+            required
           />
           <label for="otp" className="boldtext mt-4">
             Customer OTP Verification
@@ -23,9 +55,12 @@ function Configuration() {
             className="form-control my-2 py-2 w-50"
             type="text"
             placeholder="No"
+            value={otp}
+            onChange={(e)=>{setotp(e.target.value)}}
+            required
           />
           <div className="container mt-5 text-center">
-            <button className="btn btn-info py-2 w-25">Save</button>
+            <button className="btn btn-info py-2 w-25" onClick={register} >Save</button>
           </div>
         </div>
       </div>
