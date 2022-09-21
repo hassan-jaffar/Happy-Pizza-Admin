e0903738998e6ab5465511295f9481c71aad57fe
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-09-21 17:08:03 +0500

    Integrating the referral API

--- src/components/ReferralScheme.js
+++ src/components/ReferralScheme.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function ReferralScheme() {
+  const [status, setStatus] = useState(false)
+  const [new_customer, setnew_customer] = useState("")
+  const [existing_customer, setexisting_customer] = useState("")
+
+  async function register(){
+    const details = {
+      status,
+      new_customer,
+      existing_customer
+    }
+    try {
+
+      const result = await axios.post("http://localhost:5000/api/setting/referral",details).data;
+      console.log(result)
+      toast.success("Data has been saved")
+
+      setStatus(false)
+      setnew_customer("")
+      setexisting_customer("")
+
+
+
+  } catch (error) {
+      console.log(error);
+      toast.warn("Something went wrong!")
+
+  }
+  }
   return (
     <>
+       <ToastContainer />
       <h6 className="px-1">REFERRAL SCHEME</h6>
       <hr />
       <br />
@@ -18,6 +50,9 @@ function ReferralScheme() {
                 className="form-check-input"
                 type="checkbox"
                 id="referral"
+                value={status}
+                checked={status}
+                onChange={(e)=>{setStatus(e.target.checked)}}
               />
             </div>
           </div>
@@ -29,6 +64,8 @@ function ReferralScheme() {
             className="form-control my-2 py-2 w-50"
             type="number"
             placeholder="0"
+            value={new_customer}
+            onChange={(e)=>{setnew_customer(e.target.value)}}
           />
           <label for="referrer" className="mt-3">
             Referrer (Existing Customer):
@@ -38,9 +75,11 @@ function ReferralScheme() {
             className="form-control my-2 py-2 w-50"
             type="number"
             placeholder="0"
+            value={existing_customer}
+            onChange={(e)=>{setexisting_customer(e.target.value)}}
           />
           <div className="container mt-5 text-center">
-            <button className="btn btn-info py-2 w-25">Save</button>
+            <button className="btn btn-info py-2 w-25" onClick={register}>Save</button>
           </div>
         </div>
       </div>
