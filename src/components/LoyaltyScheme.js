1ab94a39e0886f3a4c8b058ea662fc187143eb5b
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-09-21 16:45:09 +0500

    Integerating the loyality API

--- src/components/LoyaltyScheme.js
+++ src/components/LoyaltyScheme.js
@@ -1,13 +1,44 @@
-import React,{useState,useEffect} from "react";
+import React,{useState} from "react";
 import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function LoyaltyScheme() {
   const [status, setStatus] = useState(false)
   const [redeem, setredeem ] = useState("")
   const [points, setpoints] = useState("")
   const [currency_points, setCurrency_points] = useState("")
+
+  async function register(){
+    const details = {
+      status,
+      redeem,
+      points,
+      currency_points
+    }
+    try {
+
+      const result = await axios.post("http://localhost:5000/api/setting/loyality",details).data;
+      console.log(result)
+      toast.success("Data has been saved")
+
+      setStatus(false)
+      setCurrency_points("")
+      setpoints("")
+      setredeem("")
+
+
+
+  } catch (error) {
+      console.log(error);
+      toast.warn("Something went wrong!")
+
+  }
+
+  }
   return (
     <>
+            <ToastContainer />
       <h6 className="px-1">LOYALTY SCHEME</h6>
       <hr />
       <br />
@@ -23,6 +54,9 @@ function LoyaltyScheme() {
                 className="form-check-input"
                 type="checkbox"
                 id="loyalty"
+                value={status}
+                checked={status}
+                onChange={(e)=>{setStatus(e.target.checked)}}
               />
             </div>
           </div>
@@ -34,6 +68,8 @@ function LoyaltyScheme() {
             className="form-control my-2 py-2 w-50"
             type="number"
             placeholder="0"
+            value={redeem}
+            onChange={(e)=>{setredeem(e.target.value)}}
           />
           <label for="currency" className="mt-3">
             Points Per Unit Currency:
@@ -43,6 +79,8 @@ function LoyaltyScheme() {
             className="form-control my-2 py-2 w-50"
             type="number"
             placeholder="i.e 5points for 1$ spending"
+            value={points}
+            onChange={(e)=>{setpoints(e.target.value)}}
           />
           <label for="perunit" className="mt-3">
             Per Unit Currency Points:
@@ -52,9 +90,11 @@ function LoyaltyScheme() {
             className="form-control my-2 py-2 w-50"
             type="number"
             placeholder="i.e give 1$ for every 10 points"
+            value={currency_points}
+            onChange={(e)=>{setCurrency_points(e.target.value)}}
           />
           <div className="container mt-5 text-center">
-            <button className="btn btn-info py-2 w-25">Save</button>
+            <button className="btn btn-info py-2 w-25" onClick={register} >Save</button>
           </div>
         </div>
       </div>
