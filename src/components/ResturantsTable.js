e840ed3123f1638e6c856aeaebc5a5e780b6a5a8
shees-butt <muhammadsheesbutt@gmail.com>
2022-09-26 18:46:35 +0500

    Styling of resturant table

--- src/components/ResturantsTable.js
+++ src/components/ResturantsTable.js
@@ -10,7 +10,9 @@ function ResturantsTable() {
     async function fetchData() {
       try {
         const data = await (
-          await axios.get("https://apinodejs.creativeparkingsolutions.com/api/admin/getallorders")
+          await axios.get(
+            "https://apinodejs.creativeparkingsolutions.com/api/admin/getallorders"
+          )
         ).data;
         setOrders(data.data);
       } catch (error) {
@@ -25,13 +27,13 @@ function ResturantsTable() {
         <div className="col-md-12 text-center">
           <h1 className="my-5 mx-4 responsiveness text-start">RESTURANTS</h1>
           <div
-            className="accordion my-2 mx-4 bs responsiveness"
+            className="accordion my-2 mx-4 responsiveness"
             id="accordionExample"
           >
             <div className="accordion-item">
               <h2 className="accordion-header" id="headingOne">
                 <button
-                  className="accordion-button boldtext"
+                  className="accordion-button"
                   type="button"
                   data-bs-toggle="collapse"
                   data-bs-target="#collapseOne"
@@ -48,136 +50,163 @@ function ResturantsTable() {
                 data-bs-parent="#accordionExample"
               >
                 <div className="accordion-body text-start my-3">
-                  <label for="daterange" className="me-1 my-1 boldtext">
-                    Date Range
-                  </label>
-                  <input
-                    id="daterange"
-                    className="me-1 my-1 py-1"
-                    placeholder="Start Date"
-                  />
-                  <input
-                    id="daterange"
-                    className="me-1 py-1"
-                    placeholder="End Date"
-                  />
-                  <label for="customerfilter" className="boldtext ms-2 my-1">
-                    Filter by Customer:
-                  </label>
-                  <input
-                    id="customerfilter"
-                    className="mx-1 py-1"
-                    placeholder="Select an option"
-                  />
-                  <button className="btn btn-primary my-1 mx-1">Export</button>
-                  <button className="btn btn-primary my-1 mx-1">Search</button>
+                  <div class="row text-start">
+                    <div class="col-md-4">
+                      <label for="daterange" className="me-1 my-1">
+                        Filter by Restaurant
+                      </label>
+                      <select
+                        class="form-select"
+                        aria-label="Default select example"
+                      >
+                        <option selected>-- Select an option --</option>
+                        <option value="1">Pontus Fish and Pizza</option>
+                        <option value="2">Golden Fry Sedgley</option>
+                        <option value="3">bkitchen</option>
+                      </select>
+                    </div>
+                    <div class="col-md-4">
+                      <label for="daterange" className="me-1 my-1">
+                        Show
+                      </label>
+                      <select
+                        class="form-select"
+                        aria-label="Default select example"
+                      >
+                        <option selected>-- Select an option --</option>
+                        <option value="1">All</option>
+                        <option value="2">Pending</option>
+                      </select>
+                    </div>
+                    <div class="col-md-4">
+                      {" "}
+                      <div style={{ marginTop: "29px" }}>
+                        <button className="btn btn-primary my-1 mx-1">
+                          Search
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        <div className="row my-5 mx-4 bs">
-            <div className="col-md-4">
-                <h4>RESTURANTS</h4>
+
+          <div className="ms-4 bs">
+          {/* 1st row  */}
+          <div className="row my-5 mx-4">
+            <div className="col-md-6 text-start mt-5">
+              <h4>RESTURANTS</h4>
             </div>
-            <div className="col-md-4"></div>
-            <div className="col-md-4">
-                <button className="btn btn-primary">
-                Add Resturant
-                </button>
+            <div className="col-md-6 text-end mt-5">
+              <button className="btn btn-primary">Add Resturant</button>
             </div>
-            <div className="row">
+            <hr style={{ padding: "2px" }} className="mt-4 text-muted"></hr>
+          </div>
+
+          {/* 2nd row for data  */}
+
+          <div className="row ms-3">
               <div className="col-md-12">
-              <div className="table-responsive">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">Name</th>
-                  <th scope="col">Logo</th>
-                  <th scope="col">Owner</th>
-                  <th scope="col">email</th>
-                  <th scope="col">Creation Date</th>
-                  <th scope="col">Active</th>
-                  <th scope="col">Live</th>
-                  <th scope="col">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
+                <div className="table-responsive">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Logo</th>
+                        <th scope="col">Owner</th>
+                        <th scope="col">email</th>
+                        <th scope="col">Creation Date</th>
+                        <th scope="col">Active</th>
+                        <th scope="col">Live</th>
+                        <th scope="col">Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {orders &&
+                        orders.map((order) => {
+                          return (
+                            <>
+                              <tr>
+                                <th scope="row">{order.ID}</th>
 
-                {orders &&
-                  orders.map((order) => {
-                    return (
-                      <>
-                        <tr>
-                            <th scope="row">
-                              
-                                {order.ID}
-                              
-                            </th>
+                                <td>
+                                  <Link
+                                    to={`/order-detail/${order.cart_Id}/${order.customer_Id}`}
+                                    style={{
+                                      textDecoration: "none",
+                                      color: "black",
+                                    }}
+                                  >
+                                    {order.house},Flat:{order.flat},
+                                    {order.street},{order.postcode},{order.town}
+                                  </Link>
+                                </td>
+                                <td>{order.DateTime}</td>
+                                <td>collection</td>
+                                <td>
+                                  {order.Orderstatus === "1" ? (
+                                    <>Pending</>
+                                  ) : order.Orderstatus === "2" ? (
+                                    <>In Process</>
+                                  ) : order.Orderstatus === "3" ? (
+                                    <>Completed</>
+                                  ) : (
+                                    <>Rejected</>
+                                  )}
+                                </td>
+                                <td>
+                                  <span class="badge bg-info primary">
+                                    cod(unpaid)
+                                  </span>
+                                </td>
+                                <td>
+                                  <span class="badge bg-danger primary">
+                                    ${order.total}
+                                  </span>
+                                </td>
+                                <td>
+                                  <div className="dropdown">
+                                    <button
+                                      type="button"
+                                      className="btn btn-outline-primary deactivatebtn blueclrname"
+                                      id="dropdownMenuButton"
+                                      data-bs-toggle="dropdown"
+                                      aria-haspopup="true"
+                                      aria-expanded="true"
+                                      // data-bs-container="body"
+                                      // data-bs-placement="bottom"
+                                      // data-bs-content="Deactivate"
+                                    >
+                                      <i className="fa-solid fa-ellipsis-vertical"></i>
+                                    </button>
+                                    <ul
+                                      class="dropdown-menu"
+                                      aria-labelledby="dropdownMenuButton"
+                                    >
+                                      <li className="dropdown-item">Edit</li>
+                                      <li className="dropdown-item">
+                                        Login as
+                                      </li>
+                                      <li className="dropdown-item">
+                                        Deactivate
+                                      </li>
+                                      <li className="dropdown-item">
+                                        Activate
+                                      </li>
+                                      <li class="dropdown-item">
+                                        <i className="fa-solid fa-ban btnicon"></i>
+                                        Delete
+                                      </li>
+                                    </ul>
+                                  </div>
+                                </td>
+                              </tr>
+                            </>
+                          );
+                        })}
 
-                            <td>
-                            <Link to={`/order-detail/${order.cart_Id}/${order.customer_Id}`} style={{ textDecoration: "none", color: "black" }}>
-
-                              {order.house},Flat:{order.flat},{order.street},
-                              {order.postcode},{order.town}
-                              </Link>
-
-                            </td>
-                            <td>{order.DateTime}</td>
-                            <td>
-                              
-                                collection
-                              
-                            </td>
-                            <td>
-                              
-                                {order.Orderstatus === "1" ? (
-                                  <>Pending</>
-                                ) : order.Orderstatus === "2" ? (
-                                  <>In Process</>
-                                ) : order.Orderstatus === "3" ? (
-                                  <>Completed</>
-                                ) : (
-                                  <>Rejected</>
-                                )}
-                              
-                            </td>
-                            <td>
-                              <span class="badge bg-info primary">
-                                cod(unpaid)
-                              </span>
-                            </td>
-                            <td>
-                            <span class="badge bg-danger primary">
-                            ${order.total}
-                              </span></td>
-                            <td>
-                            <div className="dropdown">
-                    <button
-                      type="button"
-                      className="btn btn-outline-primary deactivatebtn blueclrname"
-                      id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true"
-                      // data-bs-container="body"
-                      // data-bs-placement="bottom"
-                      // data-bs-content="Deactivate"
-                    >
-                      <i className="fa-solid fa-ellipsis-vertical"></i>
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <li className="dropdown-item">Edit</li>
-    <li className="dropdown-item">Login as</li>
-    <li className="dropdown-item">Deactivate</li>
-    <li className="dropdown-item">Activate</li>
-    <li class="dropdown-item" ><i className="fa-solid fa-ban btnicon"></i>Delete</li>
-  </ul>
-  </div>
-                            </td>
-                        </tr>
-
-                      </>
-                    );
-                  })}
-
-                {/* <tr>
+                      {/* <tr>
           <th scope="row">1017-1661245479-002</th>
           <td>House No: 1, Cross Place, DY3 1PE, Sedgley</td>
           <td>
@@ -235,14 +264,12 @@ function ResturantsTable() {
           <td>cod(unpaid)</td>
           <td>$17.93</td>
         </tr> */}
-              </tbody>
-              
-            </table>
-          </div>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
-        </div>
-
+            </div>
         </div>
       </div>
     </>
