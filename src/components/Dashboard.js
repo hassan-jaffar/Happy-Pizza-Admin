f549734818080c641fe81600602ca18e0a98de1f
shees-butt <muhammadsheesbutt@gmail.com>
2022-10-14 17:46:22 +0500

    Cross on Dashboard add

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -383,6 +383,7 @@ function Dashboard() {
                     <div className="openh1 my-5 mx-3 px-5 py-4 responsiveness">
                   <h6>
                     <i className="fa-solid fa-circle-check me-3 checkiconclr"></i>
+                    <i class="fa-sharp fa-solid fa-circle-xmark checkiconclr text-danger"></i>
                   </h6>
                   {openinfo === "true" ?(<h1 className="boldtext">Your Restaurant is Open!</h1>):(
                     <h1 className="boldtext">Your Restaurant is Closed!</h1>
